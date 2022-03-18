18202ea07259f6cb1ad64ae22b47156990792a52
Preacherek <sarna2000piotr@gmail.com>
2022-03-19 00:54:54 +0100

    Add MainScreen and GeneralStyle

--- App.tsx
+++ App.tsx
@@ -1,3 +1,10 @@
-export default function App() {
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {MainScreen} from "./front-end/general-screens/MainScreen";
 
+export default function App() {
+    return (
+      <SafeAreaProvider>
+        <MainScreen/>
+      </SafeAreaProvider>
+    );
 }
