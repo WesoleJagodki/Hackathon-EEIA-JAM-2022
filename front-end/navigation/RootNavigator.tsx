0c602261e648f9766387e307ca868bdbd9adf73b
Uszczi <mateuszpapuga24@gmail.com>
2022-03-19 15:53:05 +0100

    add registration screen

--- front-end/navigation/RootNavigator.tsx
+++ front-end/navigation/RootNavigator.tsx
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { LoadingScreen } from '../general-screens/LoadingScreen';
 import { LoginScreen } from '../general-screens/LoginScreen';
+import { RegistrationScreen } from '../general-screens/RegistrationScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,7 @@ export const RootNavigator = function RootNavigator() : JSX.Element {
         <Stack.Navigator screenOptions={{headerShown: false}}>
             <Stack.Screen name="Root" component={LoadingScreen} options={{ title: 'Welcome' }}/>
             <Stack.Screen name="LoginScreen" component={LoginScreen} options={{ title: 'Login' }} />
+            <Stack.Screen name="RegistrationScreen" component={RegistrationScreen} options={{ title: 'Registration' }} />
         </Stack.Navigator>
     );
 }
