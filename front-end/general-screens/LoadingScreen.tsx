4c9c60c7d2d066d56e52a1e1bc299d7d0b93f5a6
Preacherek <sarna2000piotr@gmail.com>
2022-03-19 12:23:09 +0100

    Fix gradient

--- front-end/general-screens/LoadingScreen.tsx
+++ front-end/general-screens/LoadingScreen.tsx
@@ -12,19 +12,19 @@ export const LoadingScreen = function({navigation}): JSX.Element {
         <View style={g_styles.container_app}>
             <LinearGradient
                 colors={['rgba(22,6,81,100)', 'transparent']}
-                style={g_styles.background}
-            />
-            <View onStartShouldSetResponder={() => navigation.navigate('LoginScreen')}>
-                <Image
-                    style={loading_screen.logo1}
-                    source={city}
-                />
-                <Image
-                    style={loading_screen.logo2}
-                    source={sub}
-                />
-                <Text style={loading_screen.h2}>closed, luxurious city in your pocket</Text>
-            </View>
+                style={g_styles.background}>
+                <View onStartShouldSetResponder={() => navigation.navigate('LoginScreen')}>
+                    <Image
+                        style={loading_screen.logo1}
+                        source={city}
+                    />
+                    <Image
+                        style={loading_screen.logo2}
+                        source={sub}
+                    />
+                    <Text style={loading_screen.h2}>closed, luxurious city in your pocket</Text>
+                </View>
+            </LinearGradient>
         </View>
     )
 }
--- front-end/general-screens/LoginScreen.tsx
+++ front-end/general-screens/LoginScreen.tsx
@@ -15,22 +15,22 @@ export const LoginScreen = function (): JSX.Element {
             <View style={g_styles.container_app}>
                 <LinearGradient
                     colors={['rgba(22,6,81,100)', 'transparent']}
-                    style={g_styles.background}
-                />
-                <Image
-                    style={login_screen.logo1}
-                    source={city}
-                />
-                <Image
-                    style={login_screen.logo2}
-                    source={sub}
-                />
-                <Text style={login_screen.h2}>closed, luxurious city in your pocket</Text>
+                    style={g_styles.background}>
+                    <Image
+                        style={login_screen.logo1}
+                        source={city}
+                    />
+                    <Image
+                        style={login_screen.logo2}
+                        source={sub}
+                    />
+                    <Text style={login_screen.h2}>closed, luxurious city in your pocket</Text>
 
-                <View style={login_screen.buttons}>
-                    <Button style={login_screen.button1} colorScheme="fuchsia" onPress={() => console.log("hello world")}>Sign in</Button>
-                    <Button style={login_screen.button2} colorScheme="fuchsia" onPress={() => console.log("hello world")}>Log in</Button>
-                </View>
+                    <View style={login_screen.buttons}>
+                        <Button style={login_screen.button1} colorScheme="fuchsia" onPress={() => console.log("hello world")}>Sign in</Button>
+                        <Button style={login_screen.button2} colorScheme="fuchsia" onPress={() => console.log("hello world")}>Log in</Button>
+                    </View>
+                </LinearGradient>
             </View>
 
         </NativeBaseProvider>
