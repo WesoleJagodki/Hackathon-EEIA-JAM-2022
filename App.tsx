b62cfb1e40284aaa1d994e03a27c8164f99b8bb9
BRZAKALA Adam <adam.brzakala@hexagon.com>
2022-03-19 11:44:55 +0100

    Use native base

--- App.tsx
+++ App.tsx
@@ -3,14 +3,12 @@ import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { LinearGradient } from 'expo-linear-gradient';
 import { g_styles } from "./front-end/general-screens/GeneralStyle";
-import {RootNavigator} from "./front-end/navigation/RootNavigator";
+import { RootNavigator } from "./front-end/navigation/RootNavigator";
 
-export default function App() : JSX.Element {
-    return (
-      <SafeAreaProvider style={g_styles.container_app}>
-          <NavigationContainer>
-            <RootNavigator/>
-          </NavigationContainer>
-      </SafeAreaProvider>
-    );
+export default function App(): JSX.Element {
+  return (
+    <NavigationContainer>
+      <RootNavigator />
+    </NavigationContainer>
+  );
 }
--- front-end/general-screens/LoginScreen.tsx
+++ front-end/general-screens/LoginScreen.tsx
@@ -1,39 +1,45 @@
-import React, {Image, Text, View, Button, Alert} from "react-native";
-import {login_screen} from "./GeneralStyle";
+import React, { Image, Text, View, Alert } from "react-native";
+import { login_screen } from "./GeneralStyle";
 
 import city from "../image/city.png";
 import sub from "../image/FANCITY.png";
 
+import { Input, NativeBaseProvider, Button } from "native-base";
+
 import { LinearGradient } from 'expo-linear-gradient';
 import { g_styles } from "./GeneralStyle";
 
-export const LoginScreen = function(): JSX.Element {
-    return(
-        <View style={g_styles.container_app}>
-            <LinearGradient
-                colors={['rgba(22,6,81,100)', 'transparent']}
-                style={g_styles.background}
-            />
-            <Image
-                style={login_screen.logo1}
-                source={city}
-            />
-            <Image
-                style={login_screen.logo2}
-                source={sub}
-            />
-            <Text style={login_screen.h2}>closed, luxurious city in your pocket</Text>
+export const LoginScreen = function (): JSX.Element {
+    return (
+        <NativeBaseProvider>
+            <View style={g_styles.container_app}>
+                <LinearGradient
+                    colors={['rgba(22,6,81,100)', 'transparent']}
+                    style={g_styles.background}
+                />
+                <Image
+                    style={login_screen.logo1}
+                    source={city}
+                />
+                <Image
+                    style={login_screen.logo2}
+                    source={sub}
+                />
+                <Text style={login_screen.h2}>closed, luxurious city in your pocket</Text>
+                <Input mx="3" placeholder="Input" w="75%" maxWidth="300px" />
 
-            <View style={login_screen.buttons}>
-                <Button
-                    title="Left button"
-                    onPress={() => Alert.alert('Left button pressed')}
-                />
-                <Button
-                    title="Right button"
-                    onPress={() => Alert.alert('Right button pressed')}
-                />
+                <View style={login_screen.buttons}>
+                    <Button
+                        title="Left button"
+                        onPress={() => Alert.alert('Left button pressed')}
+                    />
+                    <Button
+                        title="Right button"
+                        onPress={() => Alert.alert('Right button pressed')}
+                    />
+                </View>
             </View>
-        </View>
+
+        </NativeBaseProvider>
     )
 }
