1d4cd6869d5353a0fe40438cb4fa5c36cf4582a6
olasgolas11 <olekgolus02@gmail.com>
2022-03-19 14:12:44 +0100

    Fix Help and FAQ

--- front-end/general-screens/HelpFAQ.tsx
+++ front-end/general-screens/HelpFAQ.tsx
@@ -1,13 +1,10 @@
-import React, { Image, Text, View, Alert, ScrollView } from "react-native";
-import { login_screen } from "./GeneralStyle";
+import React, {Text,ScrollView,} from "react-native";
 import { Help_FAQ } from "./GeneralStyle";
-import {Input, NativeBaseProvider, Button, Box} from "native-base";
-import { Center, Square, Circle } from "native-base";
+import {NativeBaseProvider, Box} from "native-base";
+import { Center } from "native-base";
 
 import { LinearGradient } from 'expo-linear-gradient';
 import { g_styles } from "./GeneralStyle";
-import city from "../image/city.png";
-import sub from "../image/FANCITY.png";
 
 export const HelpFAQ = function (): JSX.Element {
     return (
@@ -15,13 +12,13 @@ export const HelpFAQ = function (): JSX.Element {
             <ScrollView style={g_styles.container_app}>
                 <LinearGradient
                     colors={['rgba(22,6,81,100)', 'transparent']}
-                    style={g_styles.background}>
+                    >
                     <Text style={Help_FAQ.mainText}>Help & FAQ</Text>
-                    <Center><Box style={Help_FAQ.boxText}><Text style={Help_FAQ.header}>What is Fencity?</Text><Text>ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Text></Box></Center>
-                    <Center><Box style={Help_FAQ.boxText}><Text style={Help_FAQ.header}>What is Fencity?</Text><Text>ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Text></Box></Center>
-                    <Center><Box style={Help_FAQ.boxText}><Text style={Help_FAQ.header}>What is Fencity?</Text><Text>ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Text></Box></Center>
-                    <Center><Box style={Help_FAQ.boxText}><Text style={Help_FAQ.header}>What is Fencity?</Text><Text>ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Text></Box></Center>
-                    <Center><Box style={Help_FAQ.boxText}><Text style={Help_FAQ.header}>What is Fencity?</Text><Text>ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Text></Box></Center>
+                    <Center><Box style={Help_FAQ.boxText}><Text style={Help_FAQ.header}>What is Fancity?</Text><Text>ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Text></Box></Center>
+                    <Center><Box style={Help_FAQ.boxText}><Text style={Help_FAQ.header}>What is Fancity?</Text><Text>ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Text></Box></Center>
+                    <Center><Box style={Help_FAQ.boxText}><Text style={Help_FAQ.header}>What is Fancity?</Text><Text>ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Text></Box></Center>
+                    <Center><Box style={Help_FAQ.boxText}><Text style={Help_FAQ.header}>What is Fancity?</Text><Text>ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Text></Box></Center>
+                    <Center><Box style={Help_FAQ.boxText}><Text style={Help_FAQ.header}>What is Fancity?</Text><Text>ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Text></Box></Center>
                 </LinearGradient>
 
             </ScrollView>
