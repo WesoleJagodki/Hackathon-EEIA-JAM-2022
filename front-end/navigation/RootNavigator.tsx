f774f59214c3c79a5a965b95f54403722c4d171c
BRZAKALA Adam <adam.brzakala@hexagon.com>
2022-03-19 19:21:57 +0100

    Add achievements

--- front-end/navigation/RootNavigator.tsx
+++ front-end/navigation/RootNavigator.tsx
@@ -5,6 +5,7 @@ import { LoadingScreen } from '../general-screens/LoadingScreen';
 import { LoginScreen } from '../general-screens/LoginScreen';
 import {MainScreen} from "../general-screens/MainScreen";
 import {PreferencesScreen} from "../general-screens/PreferencesScreen";
+import {AchivementsScreen} from "../general-screens/AchivementsScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -15,6 +16,7 @@ export const RootNavigator = function RootNavigator() : JSX.Element {
             <Stack.Screen name="LoginScreen" component={LoginScreen} options={{ title: 'Login' }} />
             <Stack.Screen name="MainScreen" component={MainScreen} options={{ title: 'Main' }} />
             <Stack.Screen name="PreferencesScreen" component={PreferencesScreen} options={{ title: 'Preferences' }} />
+            <Stack.Screen name="AchivementsScreen" component={AchivementsScreen} options={{ title: 'Achivements' }} />
         </Stack.Navigator>
     );
 }
